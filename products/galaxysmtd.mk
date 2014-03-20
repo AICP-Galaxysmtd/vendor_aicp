e363f9facb0f6a09a2820d0229b6262e58625947
TheWolf91 <the_wolf91@libero.it>
2014-03-20 13:59:26 +0100

    Remove not usable stuff

--- products/galaxysmtd.mk
+++ products/galaxysmtd.mk
@@ -1,4 +1,4 @@
-# Inherit AOSP device configuration for galaxys2.
+# Inherit AOSP device configuration for galaxys.
 $(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
 
 # Inherit common product files.
@@ -17,7 +17,7 @@ PRODUCT_MODEL := GT-I9000
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
 PRODUCT_RELEASE_NAME := GT-I9000
 
-# Copy i9100 specific prebuilt files
+# Copy i9000 specific prebuilt files
 PRODUCT_PACKAGES += \
     Thinkfree
 
